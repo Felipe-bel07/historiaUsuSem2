46ee2b6c4755ed6418a1f23690435c46c9763230
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-03-19 15:00:23 -0500

    Initial code

---
+++ menu.py
@@ -0,0 +1,32 @@
+from clear import *
+clear()   
+
+
+def menu():
+    to_continue = True 
+    print("*" * 43)
+    print("|", "Welcome to your Riwi inventory manager!", "|")
+    print("*" * 43)
+    print("""
+    Menu: 
+    1. Add a product
+    2. Show invetory 
+    3. Calculate stadistics 
+    4. Exit   
+    """
+    )
+    option_selected = int(input("\nPlease select the action you want to do: "))
+    # while validation:
+    #     try:
+    #         if option_selected == 1:
+            
+    #         elif option_selected == 1:
+            
+    #         elif option_selected == 3:
+            
+    #         else:
+                #to_continue = Flase 
+    #     except ValueError:
+    #         print("Please enter a valid option")
+
+menu()   
