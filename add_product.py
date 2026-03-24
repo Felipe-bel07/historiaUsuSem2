ec4d6bc3cd00491ecc9a1379000953bf3b714f01
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-03-24 14:17:00 -0500

    whil_true_correction

--- add_product.py
+++ add_product.py
@@ -1,23 +1,25 @@
 def add_product(inventory): #This is a funtion to add a new product to the inventory
+    price_continue = True
+    quantity_continue = True
     print("Please enter your product's data.")
     name = input("\nProduct's name: ")
-    while True: #This loop help to manage errors
+    while price_continue: #This loop helps managing errors
         try:
             price = float(input("Product's price: "))
             if price < 0:
                 print("The price cannot be negative")
             else:
-                break
+                price_continue = False 
         except ValueError:
             print("Please enter a valid number for price.")
 
-    while True: #This loop help to manage errors
+    while quantity_continue: #This loop help to manage errors
         try:
             quantity = int(input("Quantity: "))
             if quantity < 0:
                 print("The quantity cannot be negative")
             else: 
-                break                
+                quantity_continue = False         
         except ValueError:
             print("Please enter a valid integer for quantity.")
 
