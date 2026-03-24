a0f1889860b5f24dcf10546716672c3f6a7a435a
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-03-24 02:37:03 -0500

    add_product done

--- add_product.py
+++ add_product.py
@@ -1,1 +1,12 @@
-inventario = []
+def add_product(inventary): #This function add a new product to the invenytary 
+            print("Please enter the your product's data.")
+            name  = input("\nProduct's name: ")
+            price = float(input("Product's price: "))
+            quantity = int(input("Quantity: "))
+            product = {
+                "name": name,
+                "price": price,
+                "quantity": quantity
+            }  
+            inventary.append(product)
+            print(inventary)
