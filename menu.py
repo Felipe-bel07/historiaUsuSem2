fabac0d31af33218dfab9707b1f2e24ca83bbb3e
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-03-24 02:35:39 -0500

    Menu done

--- menu.py
+++ menu.py
@@ -1,9 +1,7 @@
 from clear import *
-clear()   
 
-
+#This is our menu function, which displays the main menu to the user. 
 def menu():
-    to_continue = True 
     print("*" * 43)
     print("|", "Welcome to your Riwi inventory manager!", "|")
     print("*" * 43)
@@ -15,18 +13,20 @@ def menu():
     4. Exit   
     """
     )
-    option_selected = int(input("\nPlease select the action you want to do: "))
-    # while validation:
-    #     try:
-    #         if option_selected == 1:
+    to_continue = True 
+    
+    #This loop help us validating that the option is entered as is expected by the program
+    while to_continue:
+        try: #Managing errors, so if a invalid value is entered the program won't crash 
+            #Telling the user to enter an option
+            option_selected = int(input("\nPlease select the action you want to do: "))
+            if option_selected > 4 or option_selected < 1: #Validating that the option selected is within the range expected 
+                print("The value entered must be within 1 to 4")
+            else:
+                to_continue = False
             
-    #         elif option_selected == 1:
-            
-    #         elif option_selected == 3:
-            
-    #         else:
-                #to_continue = Flase 
-    #     except ValueError:
-    #         print("Please enter a valid option")
+        except ValueError: 
+            print("Please enter a valid option.")
+    return option_selected
 
-menu()   
+
